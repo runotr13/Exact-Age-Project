c611723d91fbdb80c48dbe56c50201ad2e79edde
Onur Karakuzu <93216781+runotr13@users.noreply.github.com>
2022-02-09 16:36:27 +0300

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let years = document.querySelector("#yearsonur")
+let years = document.querySelector("#years")
 let months = document.querySelector("#months")
 let days = document.querySelector("#days")
 let hours = document.querySelector("#hours")
@@ -18,7 +18,7 @@ birthdayPicker.addEventListener("change",function event(){
     let day = new Date().getDate() - new Date(birthdayPicker.value).getDate() +31 ;
     document.getElementById("days").innerHTML = day;
     //SAAT
-    let hours = new Date().getHours() - new Date(birthdayPicker.value).getHours()  ;
+    let hours = new Date().getHours() - new Date(birthdayPicker.value).getHours()+23  ;
     document.getElementById("hours").innerHTML = hours;
     //DAKİKA
     let minutes = new Date().getMinutes() - new Date(birthdayPicker.value).getMinutes()  ;
@@ -31,65 +31,18 @@ birthdayPicker.addEventListener("change",function event(){
 })
 let body = document.querySelector('body')
 let loading = document.querySelector('.loading')
-birthdayPicker.addEventListener("click",()=>{
-    loading.style.display = "none";
+
+
+window.addEventListener("click",function e(){
     body.style.background = 'url("https://images3.alphacoders.com/853/thumb-1920-85305.jpg")';
 })
 
 
 
+setTimeout(function events(){
+    loading.style.display = "none";
+},500)
+events()
 
 
 
-
-
-
-
-
-
-
-// //****AY */
-// birthdayPicker.addEventListener("change",()=>{
-//     let month = new Date().getMonth() - new Date(birthdayPicker.value).getMonth()+12  ;
-//     document.getElementById("months").innerHTML = month;
-// })
-
-// //****GÜNNNNNN */
-// birthdayPicker.addEventListener("change",()=>{
-//     let day = new Date().getDate() - new Date(birthdayPicker.value).getDate()  ;
-//     document.getElementById("days").innerHTML = day;
-// })
-// birthdayPicker.addEventListener("change",()=>{
-//     let hours = new Date().getHours() - new Date(birthdayPicker.value).getHours()  ;
-//     document.getElementById("hours").innerHTML = hours;
-//     if(hours == 24){
-//         day += 1
-//     }
-// })
-
-// birthdayPicker.addEventListener("change",()=>{
-//     let minutes = new Date().getMinutes() - new Date(birthdayPicker.value).getMinutes()  ;
-//     document.getElementById("minutes").innerHTML = minutes;
-//     if(minutes==60){
-//         hours += 1
-//     }
-// })
-
-// birthdayPicker.addEventListener("change",()=>{
-//     let seconds = new Date().getSeconds() - new Date(birthdayPicker.value).getSeconds()  ;
-//     document.getElementById("seconds").innerHTML = seconds;
-//     if(seconds == 60){
-//         minutes +=1 
-//     }
-// })
-
-
-
-// var currentdate = new Date(); 
-// var datetime = "Last Sync: " + currentdate.getDate() + "/"
-//                 + (currentdate.getMonth()+1)  + "/" 
-//                 + currentdate.getFullYear() + " @ "  
-//                 + currentdate.getHours() + ":"  
-//                 + currentdate.getMinutes() + ":" 
-//                 + currentdate.getSeconds();
-// console.log(datetime);
